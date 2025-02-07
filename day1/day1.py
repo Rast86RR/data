6049763c0921b381ce71e352cb59f7e3a0ea5b69
rastsislaurudziankou <rastsislaurudziankou@gmail.com>
2025-02-07 18:11:37 +0100

    day1.2

--- day1/day1.py
+++ day1/day1.py
@@ -1,20 +1,21 @@
 import pandas as pd
 
-
+# Укажи полный путь к файлу
+file_path = "/Users/rastsislaurudziankou/Documents/my/data/csv/sales_data.csv"
 
 # Загрузка данных из CSV
-data = pd.read_csv('sales_data.csv')  # Замените 'sales_data.csv' на путь к вашему файлу
+data = pd.read_csv(file_path)
 
 # Просмотр первых строк
-print(data.head())
+#print(data.head())
 
 # Описание данных
-print(data.describe())
+#print(data.describe())
 
 # Фильтрация данных (например, выбрать только продажи в определенном регионе)
-filtered_data = data[data['region'] == 'North']
+#filtered_data = data[data['region'] == 'North']
+#print(filtered_data)
 
 # Группировка данных по колонке 'product' и суммирование по 'sales'
 grouped_data = data.groupby('product')['sales'].sum()
-
 print(grouped_data)
